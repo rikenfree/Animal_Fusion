f57a334299f8ab3a73f28f5c6377bd1a30c12fa8
Riken Sonani <rikenfree@gmail.com>
2025-07-17 10:25:50 +0530

    Add Functionality

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using DanielLochner.Assets.SimpleScrollSnap;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -17,10 +19,50 @@ public class UIManager : MonoBehaviour
     [Space(10)]
     [Header("UI SCREEN")]
     public GameObject lodingScreen;
+    public GameObject languageScreenStart;
+    public GameObject tutorialScreen;
+    public GameObject mainScreen;
     public GameObject languageScreen;
+    public GameObject settingScreen;
+    public GameObject selectModeScreen;
+    public GameObject selectTemplateScreen;
+    public GameObject pick2ItemScreen;
+    public GameObject generateScreen;
+    public GameObject generate_Screen;
+    public GameObject playVideoScreen;
 
+    [Space(10)]
+    [Header("SimpleScrollSnap")]
+    [SerializeField] SimpleScrollSnap scrollSnap;
+    [SerializeField] GameObject nextButton;
+    [SerializeField] GameObject getStartedButton;
 
+    private void Awake()
+    {
+        scrollSnap.OnPanelCentered.AddListener(OnPanelCentered);
+    }
 
+    private void OnDestroy()
+    {
+        scrollSnap.OnPanelCentered.RemoveListener(OnPanelCentered);
+    }
+
+    private void OnPanelCentered(int centeredPanel, int previousPanel)
+    {
+        int lastPanelIndex = scrollSnap.NumberOfPanels - 1;
+        // Change the sprite according to the centered panel
+        
+        if (centeredPanel == lastPanelIndex)
+        {
+            nextButton.SetActive(false);
+            getStartedButton.SetActive(true);
+        }
+        else
+        {
+            nextButton.SetActive(true);
+            getStartedButton.SetActive(false);
+        }
+    }
 
     private void Start()
     {
@@ -42,10 +84,10 @@ public class UIManager : MonoBehaviour
                 IsStartSlider = false;
                 remainingTime = 0;
                 StopSlider();
+                OnOffPanels(lodingScreen, languageScreenStart);
             }
         }
     }
-
 
     public void StartSlider()
     {
@@ -68,6 +110,19 @@ public class UIManager : MonoBehaviour
         StartSlider();
     }
 
+    public void OnOffPanels(GameObject previousPanel, GameObject nextPanel)
+    {
+        nextPanel.SetActive(true);
+        previousPanel.SetActive(false);
+    }
 
+    public void OnClickSelectedLanguage()
+    {
+        OnOffPanels(languageScreenStart, tutorialScreen);
+    }
 
+    public void OnClickGetStarted()
+    {
+        OnOffPanels(tutorialScreen, mainScreen);
+    }
 }
